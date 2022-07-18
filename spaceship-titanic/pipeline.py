90eee1310640618916c55e8dcc96ba70cf8e76d2
Lorcan Rae <lorcanrae@outlook.com>
2022-07-18 17:38:14 +0100

    created preprocessing and pipeline

--- spaceship-titanic/pipeline.py
+++ spaceship-titanic/pipeline.py
@@ -1,12 +1,11 @@
-from category_encoders import OneHotEncoder, OrdinalEncoder
 import numpy as np
 import pandas as pd
 
 from sklearn.pipeline import make_pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.compose import make_column_transformer
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import FunctionTransformer, RobustScaler, MinMaxScaler
+from sklearn.preprocessing import FunctionTransformer,RobustScaler, MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -46,7 +45,11 @@ def create_preproc_2(X_train):
     )
     return preproc_2
 
-def create_model():
+def create_preproc():
+    return make_pipeline(create_preproc_1(), create_preproc_2())
+
+def create_models():
+    # Instantiate chosen models
     svc_rbf = SVC(kernel='rbf', probability=True)
     knn_cla = KNeighborsClassifier(n_neighbors=19)
     gbe_ens = GradientBoostingClassifier(
@@ -56,13 +59,64 @@ def create_model():
         max_features='sqrt',
         subsample=0.8
         )
+    return (svc_rbf, knn_cla, gbe_ens)
+
+def create_models_params():
+    # Parameters for the gridsearch
+    svc_rbf_params = {
+        'svc__C': [30, 35, 40]
+        }
+
+    knn_cla_params = {
+        'kneighborsclassifier__n_neighbors' : [18, 19, 20],
+        }
+
+    gbe_ens_params = {
+        'gradientboostingclassifier__min_samples_split': [3, 4, 5],
+        'gradientboostingclassifier__min_samples_leaf': [1, 2, 3],
+        'gradientboostingclassifier__max_depth': [3, 4, 5],
+        'gradientboostingclassifier__subsample': [0.8],
+        'gradientboostingclassifier__max_features': ['sqrt']
+        }
+    return (svc_rbf_params, knn_cla_params, gbe_ens_params)
+
+def create_models_dict():
+    # put it all in a dictionary for easy access
+    svc_rbf, knn_cla, gbe_ens = create_models()
+    svc_rbf_params, knn_cla_params, gbm_ens_params = \
+        create_models_params()
+    models_dict = {'rbf_svc':
+            {
+                'model': svc_rbf,
+                'params': svc_rbf_params,
+                'best_score': None,
+                'best_params': None,
+                'best_estimator': None
+            },
+            'knn_cla':
+            {
+                'model': knn_cla,
+                'params': knn_cla_params,
+                'best_score': None,
+                'best_params': None,
+                'best_estimator': None
+            },
+            'gbm_ens':
+            {
+                'model': gbe_ens,
+                'params': gbm_ens_params,
+                'best_score': None,
+                'best_params': None,
+                'best_estimator': None
+            }
+        }
+    return models_dict
+
     voting_classifier = VotingClassifier(estimators=[svc_rbf, knn_cla, gbe_ens],
                                          voting='soft',
                                          weights=[1, 1, 1])
     return voting_classifier
 
-def assemble_pipe():
-    return make_pipeline(create_preproc_1, create_preproc_2, create_model)
 
 if __name__ == '__main__':
     pass
--- spaceship-titanic/trainer.py
+++ spaceship-titanic/trainer.py
@@ -4,6 +4,12 @@ import pandas as pd
 import joblib
 import datetime
 
+from sklearn.ensemble import VotingClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import make_pipeline
+
+from pipeline import create_preproc, create_models_dict
+
 # Model imports
 
 
@@ -20,23 +26,53 @@ MODEL_VERSION = 'v1'
 DATE_STAMP = datetime.now().strftime('%y%m%d')
 
 
-# Code
+class Trainer:
 
-def get_data():
-    df = pd.read_csv(f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}")
-    X_train = df.drop(columns='Transported')
-    y_train = df['Transported']
-    return (X_train, y_train)
+    def __init__(self):
+        self.data = None
+        self.X_train = None
+        self.y_train = None
+        self.model_dict = create_models_dict()
+        self.preproc = create_preproc()
+        self.best_estimators = None
+        self.final_pipe = None
 
-def assemble_pipe():
-    pass
+    def load_data(self):
+        self.data = pd.read_csv(f'gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}')
+        self.X_train = self.data.drop(columns='Transported')
+        self.y_train = self.data['Transported']
+        print(f'--- X_train shape: {self.X_train.shape}')
+
+    def gridsearchcv_tune(self, cv=5):
+        for model in self.model_dict.values():
+            search = GridSearchCV(make_pipeline(self.preproc, model['model']),
+                                  model['params'],
+                                  cv=cv,
+                                  scoring='accuracy',
+                                  n_jobs=1)
+            search.fit(self.X_train, self.y_train)
+            model['best_score'] = search.best_score_
+            model['best_params'] = search.best_params_
+            model['best_estimator'] = search.best_estimator_
+        return self
+
+    def assemble_pipe(self):
+        self.best_estimators = [model['best_estimator'] for model in self.models_dict.values()]
+        voting_classifier = VotingClassifier(
+            estimators=self.best_estimators,
+            voting='soft',
+            weights=[1, 1, 1]
+            )
+        self.final_pipe = make_pipeline(self.preproc, voting_classifier)
+        return self
+
 
 # Upload and save model to GCP
 
 def upload_model_to_gcp(model_name):
     client = storage.Client()
     bucket = client.bucket(BUCKET_NAME)
-    blob = bucket.blop(f'models/{DATE_STAMP}/')
+    blob = bucket.blob(f'models/{DATE_STAMP}/')
     blob.upload_from_filename(model_name)
 
 def save_model(model):
@@ -53,7 +89,8 @@ def save_model(model):
 
 
 if __name__ == '__main__':
-    X_train, y_train = get_data()
+    trainer = Trainer()
+    trainer.load_data()
 
     # model = train(X_train, y_train)
 
